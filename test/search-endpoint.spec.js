4592bbbd509a52e8a36ad3ce7ae3acbbc75ac831
Zachary <zachary.zboncak@gmail.com>
2020-02-15 14:35:56 -0600

    added test for happy paths of Untappd search endpoints

--- test/search-endpoint.spec.js
+++ test/search-endpoint.spec.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 
-describe('/api/search/:beer_name endpoint', () => {
-    it('returns data from a beer search', () => {
+describe('Untappd endpoints', () => {
+    it('returns data from a beer search to /api/search/:beer_name', () => {
         let beerSearchTerm = 'bourbon+county+2019';
         return supertest(app)
             .get(`/api/search/${beerSearchTerm}`)
@@ -9,5 +9,15 @@ describe('/api/search/:beer_name endpoint', () => {
             .expect(res => 
                 expect(res.text).to.exist
             )
+    })
+
+    it('returns data from a detailed beer search to /api/add/:bid', () => {
+        let bid = 2789250;
+        return supertest(app)
+            .get(`/api/add/${bid}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.text).to.exist
+            })
     })
 })
