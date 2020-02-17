87fd6229c83e3316fd02267db5e6b16784f7a7f7
Zachary Zboncak <zachary.zboncak@gmail.com>
2020-02-17 13:23:18 -0600

    configured for deployment with a database

--- src/app.js
+++ src/app.js
@@ -52,19 +52,6 @@ app.get('/api/search/:beer_name', (req, res, next) => {
   });
 })
 
-app.get('/api/add/:bid', (req, res, next) => {
-  let bid = req.params.bid;
-  let untappdUrl = `https://api.untappd.com/v4/beer/info/${bid}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&oauth_consumer_key=${CLIENT_ID}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1581543417&oauth_nonce=o4uUA6ovQTM&oauth_version=1.0&oauth_signature=+cNoi9pGpQCFryVCOHqge5KoH6o=`;
-  let options = {
-    'method': 'GET',
-    'url': untappdUrl
-  }
-  request(options, function (error, response) { 
-    if (error) throw new Error(error);
-    res.send(response.body);
-  });
-})
-
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
     JWT_SECRET: 'beer-cellar-token',
